39f516bce0f761eb0dc74c2f183021880d1745c8
LEE CHAN RIN <chali5124@gmail.com>
2017-05-24 17:18:51 +0900

    Diagnosis 데이터에 대한 리포지토리 구성

---
+++ app/Repositories/DiagnosisRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ *
+ * @Project        cargumsa
+ * @Copyright      leechanrin
+ * @Created        2017-05-24 오후 5:17:35 
+ * @Filename       DiagnosisRepository.php
+ * @Description    
+ *
+ */
+
+/**
+ * Description of DiagnosisRepository
+ *
+ * @author leechanrin
+ */
+class DiagnosisRepository {
+    //put your code here
+}
