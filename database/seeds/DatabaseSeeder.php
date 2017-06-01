26ab4277f13e32eb1a5ff50ef4836a7418cfe3d6
LEE CHAN RIN <chali5124@gmail.com>
2017-06-01 22:53:59 +0900

    Seeder add

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,19 +11,19 @@ class DatabaseSeeder extends Seeder {
      */
     public function run() {
 
-//        $this->call(BoardsSeeder::class);
-//        $this->call(CodesSeeder::class);
-        $this->call(UsersSeeder::class);
-        $this->call(PostsSeeder::class);
+       $this->call(UsersSeeder::class);
+       $this->call(PostsSeeder::class);
+       $this->call(BoardsSeeder::class);
+       $this->call(CodesSeeder::class);
 
         //  주문관련 Seeder
-//        $this->call(BrandsSeeder::class);
-//        $this->call(ModelsSeeder::class);
-//        $this->call(DetailsSeeder::class);
-//        $this->call(CarsSeeder::class);
-//        $this->call(ItemsSeeder::class);
-//        $this->call(PurchasesSeeder::class);
-//        $this->call(OrdersSeeder::class);
+       $this->call(BrandsSeeder::class);
+       $this->call(ModelsSeeder::class);
+       $this->call(DetailsSeeder::class);
+       $this->call(CarsSeeder::class);
+       $this->call(ItemsSeeder::class);
+       $this->call(PurchasesSeeder::class);
+       $this->call(OrdersSeeder::class);
 
         return;
     }
