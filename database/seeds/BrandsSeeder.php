eb7f4e30b33f12875385093ab9a4718d7626a61e
antshin <antshin72@gmail.com>
2017-06-02 16:56:18 +0900

    brands, models seeder 구성 완료함

--- database/seeds/BrandsSeeder.php
+++ database/seeds/BrandsSeeder.php
@@ -4,14 +4,91 @@ use Illuminate\Database\Seeder;
 
 class BrandsSeeder extends Seeder {
     public function run(){
-        DB::table('brands')->insert([
-            'id' => '1',
-            'name' => '기아'
-        ]);
+//        DB::table('brands')->insert([
+//            'id' => '1',
+//            'name' => '기아'
+//        ]);
+//
+//        DB::table('brands')->insert([
+//            'id' => '2',
+//            'name' => '현대'
+//        ]);
+        DB::table('brands')->insert([ 'id'=> '1', 'name' => 'BMW']);
+        DB::table('brands')->insert([ 'id'=> '2', 'name' => 'GM']);
+        DB::table('brands')->insert([ 'id'=> '3', 'name' => 'GMC']);
+        DB::table('brands')->insert([ 'id'=> '4', 'name' => '기아']);
+        DB::table('brands')->insert([ 'id'=> '5', 'name' => '기타']);
+        DB::table('brands')->insert([ 'id'=> '6', 'name' => '기타 수입차']);
+        DB::table('brands')->insert([ 'id'=> '7', 'name' => '닛산']);
+        DB::table('brands')->insert([ 'id'=> '8', 'name' => '다이하쓰']);
+        DB::table('brands')->insert([ 'id'=> '9', 'name' => '닷지']);
+        DB::table('brands')->insert([ 'id'=> '10', 'name' => '도요타']);
+        DB::table('brands')->insert([ 'id'=> '11', 'name' => '란치아']);
+        DB::table('brands')->insert([ 'id'=> '12', 'name' => '람보르기니']);
+        DB::table('brands')->insert([ 'id'=> '13', 'name' => '랜드로버']);
+        DB::table('brands')->insert([ 'id'=> '14', 'name' => '렉서스']);
+        DB::table('brands')->insert([ 'id'=> '15', 'name' => '로버']);
+        DB::table('brands')->insert([ 'id'=> '16', 'name' => '로터스']);
+        DB::table('brands')->insert([ 'id'=> '17', 'name' => '롤스로이스']);
+        DB::table('brands')->insert([ 'id'=> '18', 'name' => '르노']);
+        DB::table('brands')->insert([ 'id'=> '19', 'name' => '르노삼성']);
+        DB::table('brands')->insert([ 'id'=> '20', 'name' => '링컨']);
+        DB::table('brands')->insert([ 'id'=> '21', 'name' => '마세라티']);
+        DB::table('brands')->insert([ 'id'=> '22', 'name' => '마쯔다']);
+        DB::table('brands')->insert([ 'id'=> '23', 'name' => '맥라렌']);
+        DB::table('brands')->insert([ 'id'=> '24', 'name' => '머큐리']);
+        DB::table('brands')->insert([ 'id'=> '25', 'name' => '미니']);
+        DB::table('brands')->insert([ 'id'=> '26', 'name' => '미쓰비시']);
+        DB::table('brands')->insert([ 'id'=> '27', 'name' => '미쯔오카']);
+        DB::table('brands')->insert([ 'id'=> '28', 'name' => '벤츠']);
+        DB::table('brands')->insert([ 'id'=> '29', 'name' => '벤틀리']);
+        DB::table('brands')->insert([ 'id'=> '30', 'name' => '볼보']);
+        DB::table('brands')->insert([ 'id'=> '31', 'name' => '부가티']);
+        DB::table('brands')->insert([ 'id'=> '32', 'name' => '북기은상']);
+        DB::table('brands')->insert([ 'id'=> '33', 'name' => '뷰익']);
+        DB::table('brands')->insert([ 'id'=> '34', 'name' => '비이스만']);
+        DB::table('brands')->insert([ 'id'=> '35', 'name' => '사브']);
+        DB::table('brands')->insert([ 'id'=> '36', 'name' => '새턴']);
+        DB::table('brands')->insert([ 'id'=> '37', 'name' => '쉐보레']);
+        DB::table('brands')->insert([ 'id'=> '38', 'name' => '쉐보레/대우']);
+        DB::table('brands')->insert([ 'id'=> '39', 'name' => '스바루']);
+        DB::table('brands')->insert([ 'id'=> '40', 'name' => '스즈키']);
+        DB::table('brands')->insert([ 'id'=> '41', 'name' => '스카니아']);
+        DB::table('brands')->insert([ 'id'=> '42', 'name' => '스파이커']);
+        DB::table('brands')->insert([ 'id'=> '43', 'name' => '시트로엥']);
+        DB::table('brands')->insert([ 'id'=> '44', 'name' => '쌍용']);
+        DB::table('brands')->insert([ 'id'=> '45', 'name' => '아우디']);
+        DB::table('brands')->insert([ 'id'=> '46', 'name' => '알파로메오']);
+        DB::table('brands')->insert([ 'id'=> '47', 'name' => '애스턴마틴']);
+        DB::table('brands')->insert([ 'id'=> '48', 'name' => '어울림모터스']);
+        DB::table('brands')->insert([ 'id'=> '49', 'name' => '어큐라']);
+        DB::table('brands')->insert([ 'id'=> '50', 'name' => '오스틴']);
+        DB::table('brands')->insert([ 'id'=> '51', 'name' => '오펠']);
+        DB::table('brands')->insert([ 'id'=> '52', 'name' => '올즈모빌']);
+        DB::table('brands')->insert([ 'id'=> '53', 'name' => '웨스트필드']);
+        DB::table('brands')->insert([ 'id'=> '54', 'name' => '이스즈']);
+        DB::table('brands')->insert([ 'id'=> '55', 'name' => '인피니티']);
+        DB::table('brands')->insert([ 'id'=> '56', 'name' => '재규어']);
+        DB::table('brands')->insert([ 'id'=> '57', 'name' => '제네시스']);
+        DB::table('brands')->insert([ 'id'=> '58', 'name' => '지프']);
+        DB::table('brands')->insert([ 'id'=> '59', 'name' => '캐딜락']);
+        DB::table('brands')->insert([ 'id'=> '60', 'name' => '코닉세크']);
+        DB::table('brands')->insert([ 'id'=> '61', 'name' => '크라이슬러']);
+        DB::table('brands')->insert([ 'id'=> '62', 'name' => '테슬라']);
+        DB::table('brands')->insert([ 'id'=> '63', 'name' => '파가니']);
+        DB::table('brands')->insert([ 'id'=> '64', 'name' => '페라리']);
+        DB::table('brands')->insert([ 'id'=> '65', 'name' => '포드']);
+        DB::table('brands')->insert([ 'id'=> '66', 'name' => '포르쉐']);
+        DB::table('brands')->insert([ 'id'=> '67', 'name' => '포톤']);
+        DB::table('brands')->insert([ 'id'=> '68', 'name' => '폭스바겐']);
+        DB::table('brands')->insert([ 'id'=> '69', 'name' => '폰티악']);
+        DB::table('brands')->insert([ 'id'=> '70', 'name' => '푸조']);
+        DB::table('brands')->insert([ 'id'=> '71', 'name' => '피스커']);
+        DB::table('brands')->insert([ 'id'=> '72', 'name' => '피아트']);
+        DB::table('brands')->insert([ 'id'=> '73', 'name' => '허머']);
+        DB::table('brands')->insert([ 'id'=> '74', 'name' => '현대']);
+        DB::table('brands')->insert([ 'id'=> '75', 'name' => '혼다']);
+        DB::table('brands')->insert([ 'id'=> '76', 'name' => '홀덴']);
 
-        DB::table('brands')->insert([
-            'id' => '2',
-            'name' => '현대'
-        ]);
     }
 }
