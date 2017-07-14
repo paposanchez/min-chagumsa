c3781fefad8503c1702a66bd45444d1dd4651ce6
MinDev <minseok9106@gmail.com>
2017-07-14 14:35:21 +0900

    diagnosis 관련 원

--- app/Http/Controllers/Api/DiagnosisController.php
+++ app/Http/Controllers/Api/DiagnosisController.php
@@ -259,6 +259,7 @@ class DiagnosisController extends ApiController {
 // ]);
 
 
+//            return response()->json(json_decode($item->layout,true));
             return response()->json(json_decode($item->layout,true));
 
         }catch (Exception $e) {
@@ -308,13 +309,13 @@ class DiagnosisController extends ApiController {
 
 
             //@TODO 추후에 주문이 생성되는 시점으로 변경해야함
-            // 진단 생성            
+            // 진단 생성
             $diagnosis = new DiagnosisRepository();
             $diagnosis->prepare($order_id)->create();
 
 
             return response()->json($order);
-            
+
             // 앱에서는 간단하게
         } catch (Exception $e) {
             return abort(404, trans('diagnosis.not-found'));
@@ -354,8 +355,8 @@ class DiagnosisController extends ApiController {
             $user_id = $request->get('user_id');
 
             $validator = Validator::make($request->all(), [
-               'user_id' => 'required|exists:users,id',
-               'date' => 'required|date_format:Y-m-d'
+                'user_id' => 'required|exists:users,id',
+                'date' => 'required|date_format:Y-m-d'
             ]);
 
             if ($validator->fails()) {
@@ -370,12 +371,13 @@ class DiagnosisController extends ApiController {
             $user = User::findOrFail($user_id);
 
             $reservations = Reservation::leftJoin('orders', 'reservations.orders_id', '=', 'orders.id')
-            ->where(DB::raw("DATE_FORMAT(reservations.reservation_at, '%Y-%m-%d')"), $date)
-            ->whereNotNull("reservations.updated_at")
-            ->where('orders.garage_id', $user->user_extra->garage_id)
-            ->whereIn('orders.status_cd', [104,105])
-            ->select('reservations.*')
-            ->get(); //입고대기, 입고
+                ->where(DB::raw("DATE_FORMAT(reservations.reservation_at, '%Y-%m-%d')"), $date)
+                ->whereNotNull("reservations.updated_at")
+                ->where('orders.garage_id', $user->user_extra->garage_id)
+                ->whereIn('orders.status_cd', [104,105])
+                ->select('reservations.*')
+                ->orderBy("reservations.reservation_at", "ASC")
+                ->get(); //입고대기, 입고
 
             $returns = [];
 
@@ -421,27 +423,39 @@ class DiagnosisController extends ApiController {
      */
     public function getDiagnosisWorking(Request $request) {
         try {
-            $garage_id = $request->get('garage_id');
+
+            $user_id = $request->get('user_id');
 
             $validator = Validator::make($request->all(), [
-                'garage_id' => 'required|exists:user_extras,garage_id'
+                'user_id' => 'required|exists:users,id'
             ]);
 
             if ($validator->fails()) {
                 $errors = $validator->errors()->all();
-                throw new Exception($errors[0]);
+                return response()->json([]);
             }
 
-            $orders = Order::where('garage_id', $garage_id)
+            $user = User::findOrFail($user_id);
+
+            // $garage_id = $request->get('garage_id');
+
+            // $validator = Validator::make($request->all(), [
+            //     'garage_id' => 'required|exists:user_extras,garage_id'
+            // ]);
+
+            // if ($validator->fails()) {
+            //     $errors = $validator->errors()->all();
+            //     throw new Exception($errors[0]);
+            // }
+
+            $orders = Order::where('garage_id', $user->user_extra->garage_id)
                 ->where('status_cd', [106])
                 ->where('diagnose_at', null)
                 ->get();
 
 
             $returns = [];
-
             $diagnosis = new DiagnosisRepository();
-
             foreach($orders as $order) {
                 $returns[] = $diagnosis->prepare($order->id)->order();
             }
@@ -570,16 +584,16 @@ class DiagnosisController extends ApiController {
         $today = Reservation::where("garage_id", $user->user_extra->garage_id)->whereNotNull('updated_at')->where(DB::raw("DATE_FORMAT(reservation_at, '%Y-%m-%d')"), Carbon::today()->format('Y-m-d'))->count();
         $tomorrow = Reservation::where("garage_id", $user->user_extra->garage_id)->whereNotNull('updated_at')->where(DB::raw("DATE_FORMAT(reservation_at, '%Y-%m-%d')"), Carbon::tomorrow()->format('Y-m-d'))->count();
 
-        $today = rand(0,99);
-        $tomorrow = rand(0,99);
+        // $today = rand(0,99);
+        // $tomorrow = rand(0,99);
 
         return response()->json([
             'today' => [
-                "left" => ($today >= 10 ? $today/10 : '0'),
+                "left" => ($today >= 10 ? floor($today/10) : '0'),
                 "right" => ($today >= 10 ? $today%10 : $today)
             ],
             'tomorrow' => [
-                "left" => ($tomorrow >= 10 ? $tomorrow/10 : '0'),
+                "left" => ($tomorrow >= 10 ? floor($tomorrow/10) : '0'),
                 "right" => ($tomorrow >= 10 ? $tomorrow%10 : $tomorrow)
             ]
         ]);
