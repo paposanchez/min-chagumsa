9db672ffb49aa438ac1183f4f76f726afb89422a
antshin <antshin72@gmail.com>
2017-07-27 17:48:43 +0900

    결제 레이아웃 등록함

---
+++ resources/views/layouts/pay-base.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="ko">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+
+        <!-- Styles -->
+        {{ Html::style(Helper::assets('css/app.css')) }}
+        {{ Html::style(Helper::assets('css/vendor.css')) }}
+        
+
+        <!-- Scripts -->
+        <script>
+            window.Laravel = {!! json_encode([
+                    'csrfToken' => csrf_token(),
+            ]) !!};
+        </script>
+
+        {{--{{ Html::script(Helper::assets( 'js/app.js' )) }}--}}
+
+        @yield('content-header-script')
+
+    </head>
+
+    <body class="@yield( 'body-class' )" @yield( 'body-attr' )>
+          
+        <div id='document'>
+        
+            @yield('content-header')
+
+            @yield('content-body')
+
+            @yield('content-footer')
+
+        </div>
+
+        @yield('content-footer-script')
+    </body>
+
+</html>
