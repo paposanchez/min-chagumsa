728d86b86ba15ca0e5b799232ed4264d45379372
antshin <antshin72@gmail.com>
2017-08-03 17:37:01 +0900

    action 명 오타 수정함

--- app/Http/Controllers/Web/Mypage/OrderController.php
+++ app/Http/Controllers/Web/Mypage/OrderController.php
@@ -119,7 +119,7 @@ class OrderController extends Controller {
             ->with('success', trans('web/mypage.modify_complete'));
     }
 
-    public function cansel($order_id){
+    public function cancel($order_id){
 
 
         $order = Order::find($order_id);
