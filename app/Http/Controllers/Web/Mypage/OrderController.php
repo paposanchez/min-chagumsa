ba0ca5cbdb1b39087251793852e62e06c4bad18e
antshin <antshin72@gmail.com>
2017-08-03 17:50:06 +0900

    cancel 확인진행

--- app/Http/Controllers/Web/Mypage/OrderController.php
+++ app/Http/Controllers/Web/Mypage/OrderController.php
@@ -168,6 +168,7 @@ class OrderController extends Controller {
 
         $pay_cancel = new Client();
         $cancel_request = $pay_cancel->post($payActionUrl, $send_data);
+        dd($cancel_request);
 
 
 
