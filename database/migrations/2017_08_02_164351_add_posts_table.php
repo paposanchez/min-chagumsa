4f6a2e940849946bedf2daa4461d456fb627766f
LEE CHAN RIN <chali5124@gmail.com>
2017-08-04 16:41:24 +0900

    fix for migrate ====> fail

--- database/migrations/2017_08_02_164351_add_posts_table.php
+++ database/migrations/2017_08_02_164351_add_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPostsTable00 extends Migration
+class AddPostsTable extends Migration
 {
     /**
      * Run the migrations.
