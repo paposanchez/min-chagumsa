f41d9684ff4b2a9d336224c26736305a05ed73ed
MinDev <minseok9106@gmail.com>
2017-08-10 22:45:31 +0900

    no message

--- resources/views/web/certificate/index.blade.php
+++ resources/views/web/certificate/index.blade.php
@@ -7,47 +7,56 @@
 
 		<div class='cert_list_title'><span>{{ \Illuminate\Support\Facades\Auth::user()->name }}</span>님이 발급받은 인증서입니다.</div>
 
-		@foreach($orders as $key => $order)
-		<div class='cert_list_box'>
-			<div class='cert_box_head'>
-				<div>{{ $order->getCarFullName() }}</div>
-				<span><strong>보증기간</strong> 2017년 2월 13일까지</span>
-			</div>
-			<div class='cert_box_cont'>
-				<div class='cert_box_cont_img'>
-					<img src="http://fakeimg.pl/270x204/" alt='차량 이미지'>
-				</div>
-				<div class='cert_box_cont_info'>
-					<ul>
-						<li><label>연식</label><span>{{ $order->car->year }} 년식</span></li>
-						<li><label>차대번호</label><span>{{ $order->car->vin_number }}</span></li>
-						<li><label>차종구분</label><span>{{ $order->car->getKind->display() }} / {{ $order->car->passenger }} 인승</span></li>
-						<li><label>사용연료</label><span>{{ $order->car->getFuelType->display() }}</span></li>
-						<li><label>주행거리</label><span>{{ number_format($order->mileage) }} km</span></li>
-						<li><label>인증서발급일</label><span>{{ Carbon\Carbon::parse($order->certificates->created_at)->format('Y년 m월 d일') }}</span></li>
+		@if($orders == [])
+			@foreach($orders as $key => $order)
+				<div class='cert_list_box'>
+					<div class='cert_box_head'>
+						<div>{{ $order->getCarFullName() }}</div>
+						<span><strong>보증기간</strong> 2017년 2월 13일까지</span>
+					</div>
+					<div class='cert_box_cont'>
+						<div class='cert_box_cont_img'>
+							<img src="http://fakeimg.pl/270x204/" alt='차량 이미지'>
+						</div>
+						<div class='cert_box_cont_info'>
+							<ul>
+								<li><label>연식</label><span>{{ $order->car->year }} 년식</span></li>
+								<li><label>차대번호</label><span>{{ $order->car->vin_number }}</span></li>
+								<li><label>차종구분</label><span>{{ $order->car->getKind->display() }} / {{ $order->car->passenger }} 인승</span></li>
+								<li><label>사용연료</label><span>{{ $order->car->getFuelType->display() }}</span></li>
+								<li><label>주행거리</label><span>{{ number_format($order->mileage) }} km</span></li>
+								<li><label>인증서발급일</label><span>{{ Carbon\Carbon::parse($order->certificates->created_at)->format('Y년 m월 d일') }}</span></li>
 
-					</ul>
+							</ul>
+						</div>
+						<div class='cert_box_cont_result'>
+							<ul>
+								<li><label>산정가격</label><span><strong>{{ number_format($order->certificates->price) }} </strong>만원</span></li>
+								<li><label>차량 성능 등급</label><span><strong>{{ $order->certificates->grade }}</strong></span></li>
+							</ul>
+						</div>
+					</div>
+					<div class='cert_box_foot'>
+						{!! Form::select('open_cd', $select_open_cd, [$order->open_cd],
+                         ['class'=>'form-control btns btns2 wid20', 'required', 'data-order_id' => $order->id]) !!}
+						<div class='cert_msg'>다른 사람이 인증서를 볼 수 있도록 하려면 인증서 공개로 변경하세요</div>
+						<div class='cert_detail'>
+							<button class='btns btns_green detail' id="detail" data-order_id="{{ $order->id }}">
+								<i class="fa fa-search" ></i> 상세보기
+								<input type="text" name="order_id" id="order_id" value="{{ $order->id }}" hidden>
+							</button>
+						</div>
+					</div>
 				</div>
-				<div class='cert_box_cont_result'>
-					<ul>
-						<li><label>산정가격</label><span><strong>{{ number_format($order->certificates->price) }} </strong>만원</span></li>
-						<li><label>차량 성능 등급</label><span><strong>{{ $order->certificates->grade }}</strong></span></li>
-					</ul>
-				</div>
+			@endforeach
+		@else
+			<div class="cont_title2">
+
+				<strong>인증받은 인증서가 존재하지 않습니다.</strong>
 			</div>
-			<div class='cert_box_foot'>
-				{!! Form::select('open_cd', $select_open_cd, [$order->open_cd],
-				 ['class'=>'form-control btns btns2 wid20', 'required', 'data-order_id' => $order->id]) !!}
-				<div class='cert_msg'>다른 사람이 인증서를 볼 수 있도록 하려면 인증서 공개로 변경하세요</div>
-				<div class='cert_detail'>
-					<button class='btns btns_green detail' id="detail" data-order_id="{{ $order->id }}">
-						<i class="fa fa-search" ></i> 상세보기
-						<input type="text" name="order_id" id="order_id" value="{{ $order->id }}" hidden>
-					</button>
-				</div>
-			</div>
-		</div>
-		@endforeach
+		@endif
+
+
 	</div>
 @endsection
 
