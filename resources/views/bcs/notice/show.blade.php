9dc6508a35b73814c7ac375451a392a539590d44
antshin <antshin72@gmail.com>
2017-08-18 14:15:08 +0900

    BCS App 구성 1차 완료함. BCS 세부사항 작업해야 함

--- resources/views/bcs/notice/show.blade.php
+++ resources/views/bcs/notice/show.blade.php
@@ -17,37 +17,37 @@
                     <div class="form-group">
                         <label for="inputSubject" class="control-label col-md-2">{{ trans('admin/post.subject') }}</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" placeholder="{{ trans('admin/post.subject') }}" name="subject" id="inputSubject" value="{{ $post->subject or old('subject') }}">
+                        <input type="text" class="form-control" placeholder="{{ trans('admin/post.subject') }}" name="subject" id="inputSubject" value="{{ $post->subject or old('subject') }}">
 
-                        </div>
                     </div>
+            </div>
 
-                    <div class="form-group">
-                        <label for="inputName" class="control-label col-md-2 text-left">원본파일</label>
-                        <div class="col-md-10">
+            <div class="form-group">
+                <label for="inputName" class="control-label col-md-2 text-left">첨부파일</label>
+                <div class="col-md-10">
 
-                            <div class="input-group">
-                                <input class="form-control" readonly="" value="http://mme.lge.app/dwn/ext/020/A001/2017/08/17/ee7226ec-cf00-442d-a612-1f29c6fe84d0.pdf" id="upload-origin" type="text">
-                                <span class="input-group-btn">
-                                    <button id="dwn-orgin" data-dwn="http://mme.lge.app/dwn/ext/020/A001/2017/08/17/ee7226ec-cf00-442d-a612-1f29c6fe84d0.pdf" data-file_name="2015년_결산_및_2016년_전망보고서_(최종_4도)_160225.pdf" class="btn btn-primary" type="button">Download</button>
-                                </span>
+                    <div class="input-group">
+                        <input class="form-control" readonly="" value="http://mme.lge.app/dwn/ext/020/A001/2017/08/17/ee7226ec-cf00-442d-a612-1f29c6fe84d0.pdf" id="upload-origin" type="text">
+                        <span class="input-group-btn">
+                            <button id="dwn-orgin" data-dwn="http://mme.lge.app/dwn/ext/020/A001/2017/08/17/ee7226ec-cf00-442d-a612-1f29c6fe84d0.pdf" data-file_name="2015년_결산_및_2016년_전망보고서_(최종_4도)_160225.pdf" class="btn btn-primary" type="button">Download</button>
+                        </span>
 
-                            </div>
-                        </div>
+                    </div>
+                </div>
                     </div>
 
                     <div class="form-group">
                         <label for="inputContent" class="control-label col-md-2">{{ trans('admin/post.content') }}</label>
                         <div class="col-md-10">
-                        <textarea  class="form-control" rows="10" placeholder="{{ trans('admin/post.content') }}" name="content" id="inputContent">{{ $post->content or old('content') }}</textarea>
-
+                            <div class="panel panel-primary">
+                                    <div class="panel-body">{!! $post->content or old('content') !!}</div>
+                            </div>
                         </div>
                     </div>
 
 
 
                 </fieldset>
-
             </div>
         </div>
 
--- routes/bcs.php
+++ routes/bcs.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Http\Request;
 // After login
-//Route::group(['middleware' => ['auth', 'role:garage']], function () {
+Route::group(['middleware' => ['auth', 'role:garage']], function () {
 
     //주문
     Route::resource('order', 'OrderController');
@@ -11,5 +11,36 @@ use Illuminate\Http\Request;
     Route::get('user/bcs-info', 'UserController@bscInfo')->name('user.bcs-info');
     Route::post('user/bcs-store', 'UserController@bscStore')->name('user.bcs-store');
     Route::resource('user', 'UserController');
-    Route::get('dashboard', 'DashboardController@__invoke')->name('dashboard.index');
-//});
+    Route::get('dashboard', 'DashboardController@__invoke')->name('bcs.dashboard.index');
+});
+
+// After login in administrator's
+Route::group(['middleware' => ['guest.admin']], function () {
+    Route::get('login', function () {
+        return redirect('/');
+    });
+    // 로그인 처리
+    Route::post('login', 'Auth\LoginController@login');
+
+    // 회원정보 분실
+    // Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+    // Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    // Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+    // Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+    // Email confirmation
+    //Route::get('resend', 'Auth\RegisterController@resend');
+    //Route::get('confirm/{token}', 'Auth\RegisterController@confirm');
+    // Notifications
+    //Route::get('notifications/{user}', 'NotificationController@index');
+    //Route::put('notifications/{notification}', 'NotificationController@update');
+
+    // 로그인 페이지
+    Route::get('/', 'WelcomeController');
+
+//    Route::get('order/', 'OrderController@index');
+});
+
+
+Route::get('logout', 'Auth\LoginController@logout')->name("bcs.logout");
+Route::post('logout', 'Auth\LoginController@logout')->name("bcs.logout");
