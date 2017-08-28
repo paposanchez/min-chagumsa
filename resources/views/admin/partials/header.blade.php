1ce06d8da5f6bf774f1ac258254aa49986e34970
MinDev <minseok9106@gmail.com>
2017-08-28 12:07:01 +0900

    차검사 로고 변경( 로고 -> 텍스트 )

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -9,7 +9,8 @@
                 <a href="#" class="aside-toggle navbar-toggle navbar-icon no-border pull-left" id="aside-toggle-left"><i class="fa fa-bars"></i></a>
                 <a href="#" class="aside-toggle navbar-toggle navbar-icon no-border" id="aside-toggle-right"><i class="fa fa-ellipsis-v"></i></a>
 
-                <a href="/"  class="navbar-brand">{{ Html::image(Helper::assets('img/logo.png'), 'zlara', array('class' => 'logo', 'title'=>'logo')) }}</a>
+                {{--<a href="/"  class="navbar-brand">{{ Html::image(Helper::assets('img/logo.png'), 'zlara', array('class' => 'logo', 'title'=>'logo')) }}</a>--}}
+                <a href="/"  class="navbar-brand" style="font-size: 1.5em;margin-top: 5px;font-style: italic;font-weight: bold;">차검사</a>
             </div>
 
             <div id="navbar" class="navbar-collapse collapse">
