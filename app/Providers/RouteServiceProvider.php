fbded610cc596e0c9c186fce9ac29c9927e2c4a6
MinDev <minseok9106@gmail.com>
2017-08-30 15:50:31 +0900

    RouteServiceProvider > www 추가

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -52,8 +52,8 @@ class RouteServiceProvider extends ServiceProvider {
         Route::group([
             'middleware' => 'web',
             'namespace' => $namespace,
+            'domain' => "www.".config('app.domain'),
 //            'domain' => config('app.domain'),
-            'domain' => config('app.domain'),
                 ], function ($router) {
             require base_path('routes/web.php');
         });
