05aad74ab83955d94135699835ffc39578393bbe
antshin <antshin72@gmail.com>
2017-09-09 01:37:52 +0900

    프론트 메인작업 완료함

--- resources/views/web/layouts/default.blade.php
+++ resources/views/web/layouts/default.blade.php
@@ -19,31 +19,30 @@
 
         {{-- 헤더 메뉴 --}}
         <div id='gnb_wrap'>
-            <h1>
-                <a href='/'>
-                    {{ Html::image(Helper::theme_web( '/img/comm/head_logo.png' )) }}
-                </a>
-            </h1>
-            
-            <ul>
-                <li><a href='{{ route('information.index') }}'>차검사 소개</a></li>
-                {{--<li><a href='{{ route('order.index') }}'>인증서 신청</a></li>--}}
-                <li><a href='{{ url("/order") }}'>인증서 신청</a></li>
-                {{--<li><a href='{{ route('mypage.order.index') }}'>My 인증서</a></li>--}}
-                <li><a href='{{ route('certificate.index') }}'>My 인증서</a></li>
+            <div class='gnb_inner'>
+                <h1><a href='/'>{{ Html::image(Helper::theme_web( '/img/comm/head_logo.png' )) }}</a></h1>
+                <ul>
+                    <li><a href='{{ route('information.index') }}'>차검사 소개</a></li>
+                    {{--<li><a href='{{ route('order.index') }}'>인증서 신청</a></li>--}}
+                    <li><a href='{{ url("/order") }}'>인증서 신청</a></li>
+                    {{--<li><a href='{{ route('mypage.order.index') }}'>My 인증서</a></li>--}}
+                    <li><a href='{{ route('certificate.index') }}'>My 인증서</a></li>
 
-                <li><a href='{{ route('notice.index') }}'>고객센터</a></li>
-            </ul>
+                    <li><a href='{{ route('notice.index') }}'>고객센터</a></li>
+                </ul>
 
-            <div class='gnb_login_wrap'>
-                @if(Auth::check())
-                    <a href='{{ route('mypage.order.index') }}'>마이페이지</a>
-                    <a href='{{ route('logout') }}'>로그아웃</a>
-                @else
-                    <a href='{{ route('login') }}'>로그인</a>
-                    <a href='{{ route('register.agreement') }}'>회원가입</a>
-                @endif
+                <div class='gnb_login_wrap'>
+                    @if(Auth::check())
+                        <a href='{{ route('mypage.order.index') }}'>마이페이지</a>
+                        <a href='{{ route('logout') }}'>로그아웃</a>
+                    @else
+                        <a href='{{ route('login') }}'>로그인</a>
+                        <a href='{{ route('register.agreement') }}'>회원가입</a>
+                    @endif
+                </div>
             </div>
+
+
             {{--<form action="{{ route('search.index') }}">--}}
                 {{--<div class='gnb_search_wrap'>--}}
                     {{--<input type='text' name="q" placeholder='인증서번호 또는 차량번호로 찾기'>--}}
@@ -66,10 +65,11 @@
         {{-- 푸터 카피라이트 --}}
         <div id='foot_wrap'>
             <div id='foot_cont'>
+
                 <div class='foot_link'>
-                    <h1><a href='/'>
-                            {{ Html::image(Helper::theme_web( '/img/comm/head_logo2.png',array('style' => 'margin-top : 5px;') )) }}
-                        </a></h1>
+                    <h1>
+                        <a href='/'>{{ Html::image(Helper::theme_web( '/img/comm/head_logo.png' )) }}</a>
+                    </h1>
                     <ul>
                         <li><a class='{{ (Request::path() == "agreement/usage")? "foot_active": '' }}' href='{{ route('agreement.usage') }}'>이용약관</a></li>
                         <li><a class='{{ (Request::path() == "agreement/term")? "foot_active": '' }}' href='{{ route('agreement.term') }}'>전자금융거래약관</a></li>
@@ -77,31 +77,21 @@
                     </ul>
                 </div>
                 <div class='foot_info_wrap'>
-                    <ul class='foot_desc_wrap'>
-                        <li class='foot_desc'>
-                            <strong>주식회사 짐브러스</strong>
-                            <p>주소 : 서울시 마포구 월드컵북로 402, 10충 1026호(상암동, KGIT센터)<br>
-                               대표자 : 황병도&nbsp;&nbsp;&nbsp;&nbsp;사업자 등록번호 : 646-88-00594<br>
-
-                               통신판매업신고 : 제 2017-서울마포-1209호</p>
-                        </li>
-                        <li class='foot_desc'>
-                            <strong>고객센터</strong>
-                            <p>상담시간 : 오전 9:30분~오후 6시(토요일 및 공휴일은 휴무)<br>
-                                주소 : 서울시 마포구 월드컵북로 402, 10충 1026호(상암동, KGIT센터)<br>
-                                Tel : 1833-6889 Fax : 0505-333-6889</p>
-                        </li>
-                        <li class='foot_desc'>
-                            <strong>전자금융처리</strong>
-                            <p>Tel : 1833-6889 Fax : 0505-333-6889<br>
-                                Mail : chagumsa@jimbros.co.kr</p>
-                        </li>
-                    </ul>
+                    <div class='foot_desc_wrap'>
+                        <span>(주)짐브러스 ㅣ 대표 황병도</span>
+                        사업자번호 646-88-00594 ㅣ 통신판매업 제 2017-서울마포-1209호 ㅣ 서울특별시 마포구 월드컵북로 402 KGIT센터 1026호 ㅣ 전화 1833-6889 ㅣ  팩스 0505-333-6889 ㅣ <br />
+                        메일 cs@chagumsa.com ㅣ 업무시간 09:30~18:30(토요일 및 공휴일 휴무)
+                    </div>
                     <div class='foot_desc'>
-                        <strong>Copyright &copy; JIMBROS INC. All rights reserved.</strong>
-                        <p>(주)짐브러스의 사전 서명동의없이 차검사사이트의 일체의 정보,콘텐츠를 상업적인 목적으로 전재, 전송, 스크래핑 등 무단 사용할 수 없습니다.</p>
+                        Copyright &copy; JIMBROS Co., Ltd. All rights reserved.<br />(주)짐브러스의 사전 서명동의없이 차검사사이트의 일체의 정보,콘텐츠를 상업적인 목적으로 전재, 전송, 스크래핑 등 무단 사용할 수 없습니다.
                     </div>
                 </div>
+
+                <ul class='foot_sns'>
+                    <li><a href='' class='sns_fb'>페이스북</a></li>
+                    <li><a href='' class='sns_blog'>블로그</a></li>
+                </ul>
+
             </div>
         </div>
     @endsection
--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -4,30 +4,6 @@
 
 <div id='main_wrap'>
 
-    <div id='main_visual_wrap'>
-        <div class='main_visual'>
-            <div class='mv_cert_wrap'>
-                {{ Html::image(Helper::theme_web( '/img/main/mv_title.png')) }}
-                <div class='mv_cert_box'>
-                    <a href='{{ route('order.index') }}' class='mv_cert_btn1'>차검사 인증서 신청하기</a>
-                </div>
-                <div class='mv_cert_box' style="display: none;">
-                    {!! Form::open(['method' => 'GET', 'route' => ['search.index']]) !!}
-                    <div class='mv_cert_search_wrap'>
-                        <input type='text' name="q" placeholder='인증서번호 또는 차량번호'>
-                        <button type='submit'><i class="fa fa-search" ></i></button>
-                    </div>
-                    {!! Form::close() !!}
-                </div>
-            </div>
-            <div class='mv_report_wrap'>
-                <div class='mv_report'>
-                    <a href='#'>{{ Html::image(Helper::theme_web( '/img/main/mv_report_btn.png')) }}</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div id='main_visual2_wrap'>
         <div class='main_visual2'>
 
@@ -67,24 +43,24 @@
     <div id='main_standard_wrap'>
         <div class='main_standard'>
             <strong>
-                {{ Html::image(Helper::theme_web( '/img/main/main_standard_text.png')) }}<br>
-                중고차 거래의 <span>새로운 방법을</span>제시합니다.
+                {{ Html::image(Helper::theme_web( '/img/main/main_standard_text.png')) }}<br />
+                <span>차검사 인증서</span>로 안전하고 믿을 수 있는 중고차 거래가 가능합니다.
             </strong>
             <ul>
-                <li>차량엔진 상태</li>
-                <li>종합의견 적정가격정보</li>
-                <li>사고수리 정보</li>
-                <li>차량등록 정보</li>
-                <li>주요부품 작동상태정보</li>
-                <li>차량리콜 정보</li>
-                <li>주요패널 상태정보</li>
-                <li>차량외관 실내상태정보</li>
-                <li>주요소모품 상태정보</li>
-                <li>주요부품 작동상태정보</li>
+                <li>주요 사고</li>
+                <li>구조적 손상</li>
+                <li>침수여부</li>
+                <li>엔진 및 구동장치</li>
+                <li>전장 및 주요부품</li>
+                <li>차량 내 · 외부 수리상ㅇ태</li>
+                <li>로드테스트 기반 운행상태</li>
+                <li>차량운행이력</li>
+                <li>리콜정보</li>
+                <li>차량옵션평가</li>
             </ul>
             <div>
-                차량가치산정평가는 자동차관리법 제 58조 1항 등 자동차 가격산정에 대한 법적 권한을 보유한<br>
-                <span>자동차가술사</span>가 평가하고 가치평가 보고서를 제공합니다. 
+                자동차 품질 평가 및 가격 산정은 자동차관리법 제 58조 1항 등 자동차 가격산정에<br />
+                대한 법적 권한을 보유한 <span>자동차가술사</span>가 공인합니다.
             </div>
         </div>
     </div>
