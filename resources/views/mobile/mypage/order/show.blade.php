4146eae0f37ed6aa6d22b1e6fdee6bd992ef897a
antshin <antshin72@gmail.com>
2017-09-19 23:37:32 +0900

    주문상세 수정함

--- resources/views/mobile/mypage/order/show.blade.php
+++ resources/views/mobile/mypage/order/show.blade.php
@@ -15,20 +15,23 @@
             <div class='order_info_title'>
                 <div class='od_line'>
                     <label>주문일</label>
-                    <span>2017년 2월 21일</span>
+                    <span>{{ $order->created_at->format('Y년 m월 d일 H:i') }}</span>
                 </div>
                 <div class='od_line'>
                     <label>주문번호</label>
-                    <span>170221-21너3455</span>
+                    <span>{{ $order->getOrderNumber() }}</span>
                 </div>
             </div>
             <div class='order_info_cont'>
-                홍길동<br>
-                010-0000-0000<br>
-                폭스바겐 더 뉴 파사트 2.0 TDI
+                {{ $order->orderer_name }}<br>
+                연락처 <small class="text-muted">{{ $order->orderer_mobile }}</small></span><br>
+                차량정보 {{ $order->getCarFullName() }}
                 <div class='order_info_btn'>
-                    주문완료
-                    <button class='btns btns_navy'>취소신청</button>
+                    {{ $order->status->display() }}
+                    @if( $order->status_cd >= 101 && $order->status_cd <= 104 )
+                        <button class='btns btns_navy' id="cancel-click"
+                                data-cancel_order_id="">취소신청</button>
+                    @endif
                 </div>
             </div>
         </div>
@@ -36,96 +39,114 @@
 
         <div class='order_detail_box'>
             <div class='order_detail_title'>
-                주문자 정보
+                주문 정보
             </div>
             <div class='od_line'>
-                <label>주문자</label>
-                <span>홍길동</span>
+                <label>주문상품</label>
+                <span>{{ $order->item->name }}</span>
             </div>
             <div class='od_line'>
-                <label>휴대폰 번호</label>
+                <label>결제 방법</label>
                 <span>
-				010-1234-5678 <button class='btns btns_navy'>변경</button>
+				{{ $order->purchase->payment_type->display() }}
+			</span>
+            </div>
+            <div class='od_line'>
+                <label>결제 금액</label>
+                <span>
+				{{ number_format($order->item->price) }}원
+			</span>
+            </div>
+            <div class='od_line'>
+                <label>결제일</label>
+                <span>
+				{{ $order->purchase->updated_at->format('Y-m-d H') }}시
 			</span>
             </div>
         </div>
 
         <div class='order_detail_box'>
             <div class='order_detail_title'>
-                차량 정보 <button class='btns btns2'>변경</button>
+                차량 정보
+                @if( $order->status_cd > 100 && $order->status_cd < 105)
+                    <button class='btns btns2' data-target="{{ url("/mypage/order/change-car", [$order->id]) }}">변경</button>
+                @endif
             </div>
             <div class='od_line'>
                 <label>차량번호</label>
-                <span>12가2345</span>
+                <span>{{ $order->car_number }}</span>
             </div>
             <div class='od_line'>
                 <label>제조사</label>
-                <span>폭스바겐</span>
+                <span>{{ $order->orderCar->brand->name }}</span>
             </div>
             <div class='od_line'>
                 <label>모델</label>
-                <span>더 뉴 파사트</span>
+                <span>{{ $order->orderCar->models->name }}</span>
             </div>
             <div class='od_line'>
                 <label>세무보델</label>
-                <span>2.0 TDI</span>
+                <span>{{ $order->orderCar->detail->name }}</span>
+            </div>
+            <div class='od_line'>
+                <label>등급</label>
+                <span>{{ $order->orderCar->grade->name }}</span>
+            </div>
+            <div class='od_line'>
+                <label>침수여부</label>
+                <span>{{ $order->flooding_state_cd == 1 ? "예" : "아니요" }}</span>
+            </div>
+            <div class='od_line'>
+                <label>사고여부</label>
+                <span>{{ $order->accident_state_cd == 1 ? "예" : "아니요"  }}</span>
             </div>
             <div class='od_line'>
                 <label>옵션</label>
-                <span>전동접이 사이드미러,  선루프(순정),  가죽 시트,  전동 시트(운전석),  열선 시트(앞좌석),  에어백(운전석),  ABS 후방 감지센서,  스마트 키,  핸즈프리,  AV 시스템</span>
+                <span>{!! $features ? implode(', ',  $features) : '없음' !!}</span>
             </div>
         </div>
 
         <div class='order_detail_box'>
             <div class='order_detail_title'>
-                입고 정보 <button class='btns btns2'>변경</button>
-            </div>
-            <div class='od_line'>
-                <label>입고희망일</label>
-                <span>2016년 2월 24일 오후 1시</span>
+                예약 정보
+                @if( $order->status_cd > 100 && $order->status_cd < 105)
+                    <button class='btns btns2' data-target="{{ url("/mypage/order/change-reservation", [$order->id]) }}">변경</button>
+                @endif
             </div>
             <div class='od_line'>
                 <label>입고대리점</label>
-                <span>한스모터스<br>02-451-0788<br>서울특별시 강남구 개포로 644</span>
-            </div>
-        </div>
-
-        <div class='order_detail_box'>
-            <div class='order_detail_title'>
-                결제 정보
-            </div>
-            <div class='od_line'>
-                <label>결제수단</label>
-                <span>[신용카드] 신한카드 일시불</span>
-            </div>
-            <div class='od_line'>
-                <label>결제금액</label>
                 <span>
-				<div class='od_line'>
-					<label>수입차 가격</label>
-					<span>200,000원</span>
-				</div>
-				<div class='od_line'>
-					<label>프로모션 할인</label>
-					<span>0원</span>
-				</div>
-				<div class='od_line'>
-					<label>총 결제금액</label>
-					<span><strong>200,000</strong>원</span>
-				</div>
-			</span>
+                    {{ $order->garage->name }}<br>
+                    {{ $order->garage->user_extra->phone }}<br>
+                    {{ $order->garage->user_extra->address }}
+                </span>
+            </div>
+            <div class='od_line'>
+                <label>입고예정일</label>
+                <span>{{ $order->reservation->reservation_at->format('Y년 m월 d일 H시 i분') }}</span>
+            </div>
+            <div class='od_line'>
+                <label>예약상태</label>
+                <span>{{ $order->status->display() }}</span>
+            </div>
+            <div class='od_line'>
+                <label>예약확정</label>
+                <span>{{ $order->reservation->updated_at ? $order->reservation->updated_at->format('Y년 m월 d일 H시 i분') : '-' }}</span>
             </div>
         </div>
 
         <div class='br30'></div>
 
         <div class='ipt_line'>
-            <button class='btns btns_green' style='display:inline-block;'>주문목록 돌아가기</button>
+            <button class='btns btns_green' data-target="{{ url("/mypage/order") }}" id="order-list" style='display:inline-block;'>주문목록 돌아가기</button>
         </div>
 
 
     </div>
 
+    {!! Form::open(['route' => ["mypage.order.cancel"], 'class' =>'form-horizontal', 'method' => 'post', 'role' => 'form', 'id' => 'cancel-form']) !!}
+        <input type="hidden" name="order_id" id="cancel-order_id">
+    {!! Form::close() !!}
 @endsection
 
 
@@ -135,5 +156,31 @@
 
 @push( 'footer-script' )
 
+<script type="text/javascript">
+    $(function () {
+//        $("#order-list").on("click", function () {
+//            location.href = $(this).data("target");
+//        });
+
+        $(".btns").on("click",function () {
+            var url = $(this).data("target");
+            if(url){
+                location.href = url;
+            }
+        })
+        $("#cancel-click").on("click", function () {
+            if (confirm("해당 주문에 대한 결제를 취소하시겠습니까?")) {
+                var order_id = $(this).data("cancel_order_id");
+                if (order_id) {
+                    $("#cancel-order_id").val(order_id);
+                    $("#cancel-form").submit();
+                } else {
+                    alert("해당 주문에 대한 주문번호 오류입니다.\n새로고침 후 결제취소를 진행해 주세요.");
+                }
+
+            }
+        });
+    });
+</script>
 
 @endpush
