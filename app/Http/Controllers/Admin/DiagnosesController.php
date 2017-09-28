c2fe242a850474a0c4450079c7e57e53349c9eac
MinDev <minseok9106@gmail.com>
2017-09-28 11:55:49 +0900

    no message

--- app/Http/Controllers/Admin/DiagnosesController.php
+++ app/Http/Controllers/Admin/DiagnosesController.php
@@ -10,6 +10,7 @@ use App\Models\Diagnosis;
 use App\Models\DiagnosisFile;
 use App\Models\Order;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Code;
--- routes/admin.php
+++ routes/admin.php
@@ -62,7 +62,7 @@ Route::group(['middleware' => ['auth', 'role:admin']], function () {
 
         Route::post('/diagnosis/update-comment', 'DiagnosesController@updateComment');
         Route::post('diagnosis/update-code', 'DiagnosesController@updateCode');
-        Route::post('/diagnosis/complete', 'DiagnosesController@complete');
+        Route::post('diagnosis/complete', 'DiagnosesController@complete');
         Route::resource('diagnosis', 'DiagnosesController');
 
         // 인증서
