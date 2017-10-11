1a0da944cf2c984ca3a813c45fd9bb7a77bbfd94
antshin <antshin72@gmail.com>
2017-10-11 11:38:07 +0900

    주석 정리함

--- routes/cert.php
+++ routes/cert.php
@@ -36,54 +36,6 @@ Route::get('/thumbnail/{id?}', function($id){
 });
 Route::any('/{order_id}/{page?}/{flush?}', function ($order_id, $page = 'summary', $flush = '') {
 
-//        try
-//        {
-//                $handler = new CertificateRepository();
-//                $handler->prepare($order_id);
-//
-//                try
-//                {
-//
-//                        if(!in_array($page, ['performance', 'price', 'history', 'summary']))
-//                        {
-//                                throw new Exception('인증서가 존재하지 않습니다.');
-//                        }
-//
-//                        // html 조회
-//                        $cached_html =  $handler->cached_file($page, true);
-//
-//
-//                        if (!Storage::disk('local')->exists($cached_html) || $flush == 'cacheclear')
-//                        {
-//
-//                                // 강제로 클리어 한다
-//                                if($flush == 'cacheclear' )
-//                                {
-//                                        Storage::disk('local')->deleteDirectory($handler->cached_file($page));
-//                                }
-//
-//                                throw new Exception('인증서 캐쉬를 생성합니다.');
-//                        }
-//
-//                        return Storage::disk('local')->get($cached_html);
-//
-//                }
-//                catch (Exception $e)
-//                {
-//                        // 캐쉬 재생성후 리다이렉트
-//                        $handler->cache($page);
-//                        return redirect('/'. $order_id.'/'. $page);
-//
-//                }
-//
-//        }
-//        catch (Exception $exception)
-//        {
-//                abort(404, '인증서를 찾을 수 없습니다.');
-//
-//        }
-
-    //todo cache id 맵핑해야 함
 
     try{
 
