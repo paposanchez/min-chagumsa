7520964922eb4a2e78ba9dc96bb67c8eba729310
MinDev <minseok9106@gmail.com>
2017-11-06 16:19:24 +0900

    sms 페이지 포멧 수정

--- resources/views/message/sms/fin-diagnosis-user.blade.php
+++ resources/views/message/sms/fin-diagnosis-user.blade.php
@@ -1,5 +1,3 @@
-{{--인증서 발급 사용자 문자--}}
-@component('mail:message')
 [차검사 진단 완료]
 
 차검사 진단이 완료되었습니다. 진단 내용을 토대로 인증서가 발급될 예정이며, 상태는 차검사 홈페이지의 My 인증서에서 확인하실 수 있습니다.
@@ -8,9 +6,3 @@
 인증서 보기 : www.chagumsa.com
 
 고객센터 1833-6889
-
-@component('mail::panel')
-    This is the panel content.
-@endcomponent
-
-@endcomponent
