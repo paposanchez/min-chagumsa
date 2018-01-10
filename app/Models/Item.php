2cba8930b7275cc2de5522a9fb477be6ffc507fb
LEE CHAN RIN <chali5124@gmail.com>
2018-01-10 10:36:57 +0900

    item cache model apply

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,29 +2,26 @@
 
 namespace App\Models;
 
-use DB;
-use Illuminate\Database\Eloquent\Model;
+use App\Abstracts\Model\Cache AS CacheModel;
 
-use App\Models\Order;
-
-class Item extends Model
+class Item extends CacheModel
 {
-    protected $primaryKey = 'id';
-    protected $fillable = [
-        'name',             //상품이름
-        'price',            //가격
-        'car_sort',         //차량 종류. N- 국산, F-외산
-        'commission',       //수수료율
-        'wage',             //공임
-        'guarantee',        //보증료
-        'alliance_ratio',   //보쉬/브랜드 수수료율
-        'certi_ratio',      //기술사(H&T)수수료율
-        'self_ratio',       //짐브로스 수수료율
-        'layout',           //레이아웃
-    ];
+        protected $primaryKey = 'id';
+        protected $fillable = [
+                'name',             //상품이름
+                'price',            //가격
+                'car_sort',         //차량 종류. N- 국산, F-외산
+                'commission',       //수수료율
+                'wage',             //공임
+                'guarantee',        //보증료
+                'alliance_ratio',   //보쉬/브랜드 수수료율
+                'certi_ratio',      //기술사(H&T)수수료율
+                'self_ratio',       //짐브로스 수수료율
+                'layout',           //레이아웃
+        ];
 
-    protected $dates = [
-        'created_at', 'updated_at'
-    ];
+        protected $dates = [
+                'created_at', 'updated_at'
+        ];
 
 }
