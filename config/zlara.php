f1a723d319c4e8d22144aa1c97de64e2f0fd2add
minseok <minseok9106@gmail.com>
2018-02-14 11:25:08 +0900

    api version change

--- config/zlara.php
+++ config/zlara.php
@@ -19,7 +19,7 @@ return [
 
         'api' => [
                 'domain'        => 'api.'. config('app.domain'),
-                'version'       => '0.1.0'
+                'version'       => '0.2.0'
         ]
 
 ];
