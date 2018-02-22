13e66a9308f884ad4b5d2545bf077beb998c60bc
LEE CHAN RIN <chali5124@gmail.com>
2018-02-22 11:05:26 +0900

    front landing page change test

---
+++ resources/views/web/layouts/landing.blade.php
@@ -0,0 +1,154 @@
+{{-- LAYOUT --}}
+@extends( 'layouts.base' )
+
+@section( 'body-title', config("app.name") )
+
+@section('body-class') layout-background  @endsection
+
+@section( 'content-header-script' )
+
+        <!-- Vendor CSS -->
+        {!! Html::style('/assets/vendors/bower_components/animate.css/animate.min.css', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::style('/assets/vendors/bower_components/sweetalert2/dist/sweetalert2.min.css', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::style('/assets/vendors/bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::style('/assets/vendors/bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css') !!}
+        <!-- {!! Html::style('/assets/vendors/bower_components/fullcalendar/dist/fullcalendar.min.css', array(), env('APP_SECURE', 'true')) !!} -->
+        {!! Html::style('/assets/vendors/bower_components/dropzone/dist/min/dropzone.min.css', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::style('/assets/vendors/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::style('/assets/vendors/bower_components/nouislider/distribute/nouislider.min.css', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::style('/assets/vendors/bower_components/bootstrap-select/dist/css/bootstrap-select.css', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::style('/assets/vendors/bower_components/chosen/chosen.css', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::style('/assets/vendors/summernote/dist/summernote.css', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::style('/assets/vendors/farbtastic/farbtastic.css', array(), env('APP_SECURE', 'true')) !!}
+
+
+
+@endsection
+
+@section( 'content-header' )
+
+@include('flash::message')
+@endsection
+
+@section( 'content-body' )
+<section id="main">
+
+        {{-- 본문의 사이드 --}}
+        @includeIf( 'web.partials.left' )
+
+        @hasSection('breadcrumbs')
+        <div class="container-wrapper">
+
+                <div class="container">
+
+                        @yield( 'breadcrumbs' )
+
+                </div>
+        </div>
+        @endif
+
+        @yield( 'content' )
+
+</section>
+
+@endsection
+
+@section( 'content-footer' )
+<!-- Page Loader -->
+<div class="page-loader">
+    <div class="preloader pls-blue">
+        <svg class="pl-circular" viewBox="25 25 50 50">
+            <circle class="plc-path" cx="50" cy="50" r="20" />
+        </svg>
+
+        <p>로딩중...</p>
+    </div>
+</div>
+
+<!-- Older IE warning message -->
+<!--[if lt IE 9]>
+    <div class="ie-warning">
+        <h1 class="c-white">Warning!!</h1>
+        <p>You are using an outdated version of Internet Explorer, please upgrade <br/>to any of the following web browsers to access this website.</p>
+        <div class="iew-container">
+            <ul class="iew-download">
+                <li>
+                    <a href="http://www.google.com/chrome/">
+                        <img src="img/browsers/chrome.png" alt="">
+                        <div>Chrome</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.mozilla.org/en-US/firefox/new/">
+                        <img src="img/browsers/firefox.png" alt="">
+                        <div>Firefox</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.opera.com">
+                        <img src="img/browsers/opera.png" alt="">
+                        <div>Opera</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.apple.com/safari/">
+                        <img src="img/browsers/safari.png" alt="">
+                        <div>Safari</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+                        <img src="img/browsers/ie.png" alt="">
+                        <div>IE (New)</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <p>Sorry for the inconvenience!</p>
+    </div>
+<![endif]-->
+@endsection
+
+
+@section( 'content-footer-script' )
+
+
+        <!-- Javascript Libraries -->
+        {!! Html::script('/assets/vendors/bower_components/flot/jquery.flot.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/flot/jquery.flot.resize.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/flot.curvedlines/curvedLines.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/sparklines/jquery.sparkline.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/jquery.easy-pie-chart/dist/jquery.easypiechart.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/moment/min/moment.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/fullcalendar/dist/fullcalendar.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/simpleWeather/jquery.simpleWeather.min.js', array(), env('APP_SECURE', 'true')) !!}
+
+        {!! Html::script('/assets/vendors/bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/Waves/dist/waves.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bootstrap-growl/bootstrap-growl.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/sweetalert2/dist/sweetalert2.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/bootstrap-select/dist/js/bootstrap-select.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/nouislider/distribute/nouislider.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/typeahead.js/dist/typeahead.bundle.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/dropzone/dist/min/dropzone.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/summernote/dist/summernote-updated.min.js', array(), env('APP_SECURE', 'true')) !!}
+
+        {!! Html::script('/assets/vendors/bower_components/chosen/chosen.jquery.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/bower_components/jquery-mask-plugin/dist/jquery.mask.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/fileinput/fileinput.min.js', array(), env('APP_SECURE', 'true')) !!}
+        {!! Html::script('/assets/vendors/farbtastic/farbtastic.min.js', array(), env('APP_SECURE', 'true')) !!}
+
+
+        <!-- Placeholder for IE9 -->
+        <!--[if IE 9 ]>
+        {!! Html::script('/assets/vendors/bower_components/jquery-placeholder/jquery.placeholder.min.js', array(), env('APP_SECURE', 'true')) !!}
+        <![endif]-->
+
+
+        {{-- application script --}}
+        {{ Html::script(mix( '/assets/js/app.front.js'), array(), env('APP_SECURE', 'true')) }}
+
+        @stack( 'footer-script' )
+
+@endsection
--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -1,199 +1,33 @@
-@extends( 'web.layouts.default' )
+@extends( 'web.layouts.landing' )
+
+
+@section('body-attr') style="background-image: url('assets/img/background-landing.jpeg'); " @endsection
+
 
 @section( 'content' )
 
-{{-- 메인 배너 --}}
-@include( 'web.partials.hometainer' )
+{{-- 헤더 메뉴 --}}
+@includeIf( 'web.partials.header' )
 
-<section id="content" class="content-alt">
+{{-- 메인 배너 --}}
+<section id="content" class="">
 
         <div class="container">
 
-                <div class="card">
-                        <div class="card-header">
-                                <h2>Sales Statistics <small>Vestibulum purus quam scelerisque, mollis nonummy metus</small></h2>
-
-                                <ul class="actions">
-                                        <li>
-                                                <a href="">
-                                                        <i class="zmdi zmdi-refresh-alt"></i>
-                                                </a>
-                                        </li>
-                                        <li>
-                                                <a href="">
-                                                        <i class="zmdi zmdi-download"></i>
-                                                </a>
-                                        </li>
-                                        <li class="dropdown">
-                                                <a href="" data-toggle="dropdown">
-                                                        <i class="zmdi zmdi-more-vert"></i>
-                                                </a>
-
-                                                <ul class="dropdown-menu dropdown-menu-right">
-                                                        <li>
-                                                                <a href="">Change Date Range</a>
-                                                        </li>
-                                                        <li>
-                                                                <a href="">Change Graph Type</a>
-                                                        </li>
-                                                        <li>
-                                                                <a href="">Other Settings</a>
-                                                        </li>
-                                                </ul>
-                                        </li>
-                                </ul>
-                        </div>
-
-                        <div class="card-body">
-                                <div class="chart-edge">
-                                        <div id="curved-line-chart" class="flot-chart "></div>
-                                </div>
+                <div class="row">
+                        <div class="col-lg-6">
+                                <h1 class="text-white text-lighter m-t-20">Car Inspection Valueation Warranty</h1>
+                                <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptates rerum eveniet sapiente repellat esse, doloremque quod recusandae deleniti nostrum assumenda vel beatae sed aut modi nesciunt porro quisquam voluptatem.</p>
                         </div>
                 </div>
-
-                <div class="mini-charts">
-                        <div class="row">
-                                <div class="col-sm-6 col-md-3">
-                                        <div class="mini-charts-item bgm-lightgreen">
-                                                <div class="clearfix">
-                                                        <div class="chart stats-bar"></div>
-                                                        <div class="count">
-                                                                <small>Website Traffics</small>
-                                                                <h2>987,459</h2>
-                                                        </div>
-                                                </div>
-                                        </div>
-                                </div>
-
-                                <div class="col-sm-6 col-md-3">
-                                        <div class="mini-charts-item bgm-purple">
-                                                <div class="clearfix">
-                                                        <div class="chart stats-bar-2"></div>
-                                                        <div class="count">
-                                                                <small>Website Impressions</small>
-                                                                <h2>356,785K</h2>
-                                                        </div>
-                                                </div>
-                                        </div>
-                                </div>
-
-                                <div class="col-sm-6 col-md-3">
-                                        <div class="mini-charts-item bgm-orange">
-                                                <div class="clearfix">
-                                                        <div class="chart stats-line"></div>
-                                                        <div class="count">
-                                                                <small>Total Sales</small>
-                                                                <h2>$ 458,778</h2>
-                                                        </div>
-                                                </div>
-                                        </div>
-                                </div>
-
-                                <div class="col-sm-6 col-md-3">
-                                        <div class="mini-charts-item bgm-bluegray">
-                                                <div class="clearfix">
-                                                        <div class="chart stats-line-2"></div>
-                                                        <div class="count">
-                                                                <small>Support Tickets</small>
-                                                                <h2>23,856</h2>
-                                                        </div>
-                                                </div>
-                                        </div>
-                                </div>
-                        </div>
-                </div>
-
-                <div class="dash-widgets">
-                        <div class="row">
-                                <div class="col-md-4 col-sm-6">
-                                        <div id="site-visits" class="dw-item bgm-teal">
-                                                <div class="dwi-header">
-                                                        <div class="p-30">
-                                                                <div class="dash-widget-visits"></div>
-                                                        </div>
-
-                                                        <div class="dwih-title">For the past 30 days</div>
-                                                </div>
-
-                                                <div class="list-group lg-even-white">
-                                                        <div class="list-group-item media sv-item">
-                                                                <div class="pull-right">
-                                                                        <div class="stats-bar"></div>
-                                                                </div>
-                                                                <div class="media-body">
-                                                                        <small>Page Views</small>
-                                                                        <h3>47,896,536</h3>
-                                                                </div>
-                                                        </div>
-
-                                                        <div class="list-group-item media sv-item">
-                                                                <div class="pull-right">
-                                                                        <div class="stats-bar-2"></div>
-                                                                </div>
-                                                                <div class="media-body">
-                                                                        <small>Site Visitors</small>
-                                                                        <h3>24,456,799</h3>
-                                                                </div>
-                                                        </div>
-
-                                                        <div class="list-group-item media sv-item">
-                                                                <div class="pull-right">
-                                                                        <div class="stats-line"></div>
-                                                                </div>
-                                                                <div class="media-body">
-                                                                        <small>Total Clicks</small>
-                                                                        <h3>13,965</h3>
-                                                                </div>
-                                                        </div>
-                                                </div>
-                                        </div>
-                                </div>
-
-                                <div class="col-md-4 col-sm-6">
-                                        <div id="pie-charts" class="dw-item bgm-cyan c-white">
-                                                <div class="">
-                                                        <div class="dwi-header">
-                                                                <div class="dwih-title">Email Statistics</div>
-                                                        </div>
-
-                                                        <div class="clearfix"></div>
-
-                                                        <div class="text-center p-20 m-t-25">
-                                                                <div class="easy-pie main-pie" data-percent="75">
-                                                                        <div class="percent">45</div>
-                                                                        <div class="pie-title">Total Emails Sent</div>
-                                                                </div>
-                                                        </div>
-                                                </div>
-
-                                                <div class="p-t-15 p-b-20 text-center">
-                                                        <div class="easy-pie sub-pie-1" data-percent="56">
-                                                                <div class="percent">56</div>
-                                                                <div class="pie-title">Bounce Rate</div>
-                                                        </div>
-                                                        <div class="easy-pie sub-pie-2" data-percent="84">
-                                                                <div class="percent">84</div>
-                                                                <div class="pie-title">Total Opened</div>
-                                                        </div>
-                                                        <div class="easy-pie sub-pie-2" data-percent="21">
-                                                                <div class="percent">21</div>
-                                                                <div class="pie-title">Total Ignored</div>
-                                                        </div>
-                                                </div>
-
-                                        </div>
-                                </div>
-
-                                <div class="col-md-4 col-sm-6">
-                                        <div class="dw-item bgm-lime">
-                                                <div id="weather-widget"></div>
-                                        </div>
-                                </div>
-                        </div>
-                </div>
-
         </div>
 </section>
+
+
+{{-- 푸터 카피라이트 --}}
+{{-- @includeIf( 'web.partials.footer' ) --}}
+
+
 @endsection
 
 
