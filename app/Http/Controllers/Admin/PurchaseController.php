ce946e4731898177c20a174f4a11ed84386a1b25
minseok <minseok9106@gmail.com>
2018-02-23 11:17:29 +0900

    팝업만 임시 구성

--- app/Http/Controllers/Admin/PurchaseController.php
+++ app/Http/Controllers/Admin/PurchaseController.php
@@ -9,130 +9,40 @@ use App\Models\Role;
 use App\Models\Permission;
 use DB;
 
-class PurchaseController extends Controller {
+class PurchaseController extends Controller
+{
 
-        /**
-        * Display a listing of the resource.
-        *
-        * @return \Illuminate\Http\Response
-        */
-        public function index(Request $request) {
-                $entrys = Purchase::orderBy('id', 'DESC')->paginate(25);
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $entrys = Purchase::orderBy('id', 'DESC')->paginate(25);
 
-                return view('admin.purchase.index', compact('entrys'));
-        }
+        return view('admin.purchase.index', compact('entrys'));
+    }
 
-        /**
-        * Show the form for creating a new resource.
-        *
-        * @return \Illuminate\Http\Response
-        */
-        public function create() {
-                $permissions = Permission::get();
-                return view('admin.permission.create', compact('permissions'));
-        }
+    public function update(Request $request){
+        dd('update');
+    }
 
-        /**
-        * Store a newly created resource in storage.
-        *
-        * @param  \Illuminate\Http\Request  $request
-        * @return \Illuminate\Http\Response
-        */
-        public function store(Request $request) {
+    public function getDetail(Request $request){
+        $id = $request->get('id');
+        $purhcase = Purchase::findOrFail($id);
 
-                $this->validate($request, [
-                        'name' => 'required|min:3|unique:permissions,name',
-                        'display_name' => 'required',
-                        'role' => 'required',
-                ], [], [
-                        'name' => trans('admin/permission.name'),
-                        'display_name' => trans('admin/permission.display_name'),
-                        'description' => trans('admin/permission.description'),
-                        'role' => trans('admin/permission.role'),
-                ]);
-
-                $permission = new Permission();
-                $permission->name = $request->input('name');
-                $permission->display_name = $request->input('display_name');
-                $permission->description = $request->input('description');
-                $permission->save();
-
-                if ($request->input('role')) {
-                        foreach ($request->input('role') as $value) {
-                                $permission->attachPermission($value);
-                        }
-                }
-
-                return redirect()->route('permission.edit', [$permission->id])
-                ->with('success', trans("admin/permission.created"));
-        }
-
-        /**
-        * Show the form for editing the specified resource.
-        *
-        * @param  int  $id
-        * @return \Illuminate\Http\Response
-        */
-        public function edit($id) {
-
-                $permission = Permission::find($id);
-                $permissions = Permission::get();
-
-                $rolePermissions = DB::table("permission_role")
-                ->where("permission_role.role_id", $id)
-                ->pluck('permission_role.permission_id', 'permission_role.permission_id');
-
-                return view('admin.permission.edit', compact('permission', 'permissions', 'rolePermissions'));
-        }
-
-        /**
-        * Update the specified resource in storage.
-        *
-        * @param  \Illuminate\Http\Request  $request
-        * @param  int  $id
-        * @return \Illuminate\Http\Response
-        */
-        public function update(Request $request, $id) {
-                $this->validate($request, [
-                        //            'name' => 'required|min:3|unique:permissions,name',
-                        'display_name' => 'required',
-                        'role' => 'required',
-                ], [], [
-                        //            'name' => trans('admin/permission.name'),
-                        'display_name' => trans('admin/permission.display_name'),
-                        'description' => trans('admin/permission.description'),
-                        'role' => trans('admin/permission.role')
-                ]);
-
-                $permission = Permission::find($id);
-                $permission->display_name = $request->input('display_name');
-                $permission->description = $request->input('description');
-                $permission->save();
-
-
-                if ($request->input('role')) {
-                        DB::table("permission_role")->where("permission_role.role_id", $id)
-                        ->delete();
-
-                        foreach ($request->input('permission') as $key => $value) {
-                                $permission->attachPermission($value);
-                        }
-                }
-
-                return redirect()->back()
-                ->with('success', 'Role updated successfully');
-        }
-
-        /**
-        * Remove the specified resource from storage.
-        *
-        * @param  int  $id
-        * @return \Illuminate\Http\Response
-        */
-        public function destroy($id) {
-                DB::table("roles")->where('id', $id)->delete();
-                return redirect()->route('role.index')
-                ->with('success', 'Role deleted successfully');
-        }
+        return response()->json(
+            [
+                'status_cd' => $purhcase->status_cd,
+                'id' => $purhcase->id,
+                'type' => $purhcase->tyoe,
+                'amount' => $purhcase->amount,
+                'refund_name' => $purhcase->refund_name,
+                'refund_account' => $purhcase->refund_account,
+                'refund_bank' => $purhcase->refund_bank
+            ]
+        );
+    }
 
 }
--- resources/views/admin/purchase/index.blade.php
+++ resources/views/admin/purchase/index.blade.php
@@ -1,132 +1,234 @@
 @extends( 'admin.layouts.default' )
 
 @section( 'content' )
-<section id="content">
+    <section id="content">
         <div class="container">
-                <div class="card">
-                        <div class="card-header ch-alt">
-                                <h2>결제관리
-                                        <small>결제관리</small>
-                                </h2>
-                        </div>
-
-                        <div class="card-body">
-                                <table class="table text-center">
-                                        <colgroup>
-                                                <col width="8%">
-                                                <col width="10%" class="">
-                                                <col width="10%" class="">
-                                                <col width="10%" class="">
-                                                <col width="10%">
-                                                <col width="20%">
-                                                <col width="15%">
-                                                <col width="*">
-                                        </colgroup>
-
-                                        <thead>
-                                                <tr class="">
-                                                        <th class="text-center">상태</th>
-                                                        <th class="text-center">주문번호</th>
-                                                        <th class="text-center">결제번호</th>
-                                                        <th class="text-center">결제방법</th>
-                                                        <th class="text-center">결제금액</th>
-                                                        <th class="text-center">PG</th>
-                                                        <!-- <th class="text-center">결제내역</th> -->
-                                                        <th class="text-center">결제일</th>
-                                                        <th class="text-center">Remarks</th>
-                                                </tr>
-                                        </thead>
-
-
-                                        <tbody>
-
-                                                @unless(count($entrys) >0)
-                                                <tr>
-                                                        <td colspan="5" class="no-result">{{ trans('common.no-result') }}</td>
-                                                </tr>
-                                                @endunless
-
-                                                @foreach ($entrys as $key => $data)
-                                                <tr>
-                                                        <td class="text-center">
-                                                                @component('components.badge', [
-                                                                'code' => $data->status_cd,
-                                                                'color' =>[
-                                                                '100' => 'default',
-                                                                '102' => 'success',
-                                                                '112' => 'success',
-                                                                '113' => 'warning',
-                                                                '114' => 'info',
-                                                                '115' => 'primary',
-                                                                '116' => 'danger'
-                                                                ]])
-                                                                {{ $data->status->display() }}
-                                                                @endcomponent
-                                                        </td>
-
-                                                        <td>{{ $data->order->id }}</td>
-                                                        <td>{{ $data->id }}</td>
-
-
-                                                        <td>{{ $data->payment_type->display() }}</td>
-
-                                                        <td>{{ $data->amount }}</td>
-
-                                                        <td>{{ $data->pg }}</td>
-
-
-
-
-                                                        <!-- <td class="text-center">
-                                                                <a href="/user/{{ $data->order->orderer_id }}/edit">{{ $data->order->orderer ? $data->order->orderer->email : '-' }}</a>
-                                                                <br/>
-                                                                <small class="text-warning">{{ $data->order->orderer_mobile }}</small>
-                                                        </td> -->
-
-
-
-                                                        <!-- <td class="text-center">
-                                                                @foreach($data->order->orderItem as $order_item)
-                                                                @if(\Illuminate\Support\Facades\Auth::user()->hasRole('admin'))
-                                                                <a href="/config/item/{{ $order_item->item->id }}/edit" data-toggle="tooltip" title="{{ $order_item->item->type->display() }}" class="badge">{{ $order_item->item->typeString() }}</a>
-                                                                @else
-                                                                <span data-toggle="tooltip" title="{{ $order_item->item->type->display() }}" class="badge">{{ $order_item->item->typeString() }}</span>
-                                                                @endif
-
-                                                                @endforeach
-                                                                <br/>
-                                                                <small class="text-warning">{{ $data->payment_type->display() }}</small>
-                                                        </td> -->
-
-                                                        <td class="text-center">{{ $data->updated_at }}</td>
-
-                                                        <td class="text-center">
-
-                                                                <a href="{{ route("purchase.show", [$data->id]) }}" class="btn btn-default btn-icon waves-effect waves-float" data-toggle="tooltip" title="결제정보 상세보기"><i class="zmdi zmdi-search-in-page"></i></a>
-
-
-
-                                                        </td>
-                                                </tr>
-                                                @endforeach
-
-                                        </tbody>
-                                </table>
-                        </div>
-
-                        {{--page navigation--}}
-                        {!! $entrys->render() !!}
-
+            <div class="card">
+                <div class="card-header ch-alt">
+                    <h2>결제관리
+                        <small>결제관리</small>
+                    </h2>
                 </div>
 
+                <div class="card-body">
+                    <table class="table text-center">
+                        <colgroup>
+                            <col width="8%">
+                            <col width="10%" class="">
+                            <col width="10%" class="">
+                            <col width="10%" class="">
+                            <col width="10%">
+                            <col width="20%">
+                            <col width="15%">
+                            <col width="*">
+                        </colgroup>
+
+                        <thead>
+                        <tr class="">
+                            <th class="text-center">상태</th>
+                            <th class="text-center">주문번호</th>
+                            <th class="text-center">결제번호</th>
+                            <th class="text-center">결제방법</th>
+                            <th class="text-center">결제금액</th>
+                            <th class="text-center">PG</th>
+                            <!-- <th class="text-center">결제내역</th> -->
+                            <th class="text-center">결제일</th>
+                            <th class="text-center">Remarks</th>
+                        </tr>
+                        </thead>
+
+
+                        <tbody>
+
+                        @unless(count($entrys) >0)
+                            <tr>
+                                <td colspan="5" class="no-result">{{ trans('common.no-result') }}</td>
+                            </tr>
+                        @endunless
+
+                        @foreach ($entrys as $key => $data)
+                            <tr>
+                                <td class="text-center">
+                                    @component('components.badge', [
+                                    'code' => $data->status_cd,
+                                    'color' =>[
+                                    '100' => 'default',
+                                    '102' => 'success',
+                                    '112' => 'success',
+                                    '113' => 'warning',
+                                    '114' => 'info',
+                                    '115' => 'primary',
+                                    '116' => 'danger'
+                                    ]])
+                                        {{ $data->status->display() }}
+                                    @endcomponent
+                                </td>
+
+                                <td>{{ $data->order->id }}</td>
+                                <td>{{ $data->id }}</td>
+
+
+                                <td>{{ $data->payment_type->display() }}</td>
+
+                                <td>{{ $data->amount }}</td>
+
+                                <td>{{ $data->pg }}</td>
+
+                                <td class="text-center">{{ $data->updated_at }}</td>
+
+                                <td class="text-center">
+
+                                    <a href="{{ route("purchase.show", [$data->id]) }}"
+                                       class="btn btn-default btn-icon waves-effect waves-float detail"
+                                       data-toggle="modal"
+                                       data-target="#detailModal" data-id="{{ $data->id }}"
+                                       title="결제정보 상세보기"><i class="zmdi zmdi-search-in-page"></i></a>
+
+                                </td>
+                            </tr>
+                        @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+
+                {{--page navigation--}}
+                {!! $entrys->render() !!}
+
+            </div>
+
         </div>
-</section>
+    </section>
+
+    <!-- Car Modal -->
+    <div id="detailModal" class="modal fade" role="dialog">
+        <div class="modal-dialog modal-lg">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="card">
+                    <div class="card-header bgm-blue m-b-20">
+                        <h2>결제 상세내용
+                            <small></small>
+                        </h2>
+                        <ul class="actions actions-alt">
+                            <li class="dropdown">
+                                <a href="#">
+                                    <i class="zmdi zmdi-close" data-dismiss="modal"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="card-body card-padding">
+                        <div class="modal-body">
+                            {!! Form::open(['method' => 'PATCH','route' => ['purchase.update', 'id' => 0], 'class'=>'form-horizontal', 'id'=>'carForm', 'enctype'=>"multipart/form-data"]) !!}
+                                <input type="hidden" name="purchase_id" id="purchase_id" value="">
+                                <div class="card-header ch-alt">
+                                    <h2>주문관리</h2>
+                                </div>
+                                <table class="table text-center">
+                                    <colgroup>
+                                        <col width="8%">
+                                        <col width="8%">
+                                        <col width="10%">
+                                        <col width="12%">
+                                        <col width="10%">
+                                        <col width="10%">
+                                        <col width="*">
+                                        <col width="10%">
+                                    </colgroup>
+                                    <thead>
+                                    <tr class="">
+                                        <th class="text-center">상태</th>
+                                        <th class="text-center">ID</th>
+                                        <th class="text-center">결제타입</th>
+                                        <th class="text-center">결제금액</th>
+                                        <th class="text-center">예금주</th>
+                                        <th class="text-center">은행</th>
+                                        <th class="text-center">계좌번호</th>
+                                        <th class="text-center">Remarks</th>
+                                    </tr>
+                                    </thead>
+
+                                    <tbody id="payment">
+                                    <tr>
+                                        <td>
+                                            1
+                                        </td>
+
+                                        <td>
+                                            2
+                                        </td>
+
+                                        <td>
+                                            3
+                                        </td>
+
+                                        <td>
+                                            4
+                                        </td>
+
+                                        <td>
+                                            5
+                                        </td>
+
+                                        <td>
+                                            6
+                                        </td>
+
+                                        <td>
+                                            7
+                                        </td>
+
+                                        <td>
+                                            <button
+                                               class="btn btn-default btn-icon waves-effect waves-float"
+                                               data-toggle="tooltip" title="변경하기"><i
+                                                        class="zmdi zmdi-refresh-sync"></i></button>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            {!! Form::close() !!}
+                        </div>
+
+                        <div class="modal-footer">
+                            {{--<button type="submit" class="btn btn-success" id="">변경</button>--}}
+                            {{--<button type="button" class="btn btn-default" data-dismiss="modal">취소</button>--}}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
-
-@push( 'header-script' )
-@endpush
-
-
 @push( 'footer-script' )
+    <script type="text/javascript">
+        $(document).on('click', '.detail', function () {
+            var id = $(this).data('id');
+
+            $.ajax({
+                type: 'get',
+                dataType: 'json',
+                url: '/purchase/get-detail',
+                data: {
+                    'id': id
+                },
+                success: function (data) {
+                    $('#payment').html();
+                    $.each(data, function(key, value){
+
+                    });
+
+                },
+                error: function (data) {
+                    // alert(JSON.stringify(data));
+                }
+            });
+        });
+
+    </script>
+
 @endpush
